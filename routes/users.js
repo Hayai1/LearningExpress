cc4a1ea629edc8fba6844b88b4f985b7947d26be
Dylan <dylanpjhough@gmail.com>
2023-09-29 12:02:13 +0100

    added query params

--- routes/users.js
+++ routes/users.js
@@ -1,60 +1,50 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 
-
-router.use(logger);//runs logger on all requests
+router.use(logger)//runs logger on all requests
 
 router.get("/", (req, res) => {
-    res.send("user list");
-
-
-});
+    console.log(req.query.name)
+    res.send("user list")
+})
 
 router.get("/new", (req, res) => {
-    res.render("users/new", { firstName: "Test" });
-});
+    res.render("users/new")
+})
 
 router.post("/", (req, res) => {
-    const isValid = true;
+    const isValid = true
     if (isValid) {
         users.push({firstName : req.body.firstName})//adds the new user to the array
-        res.redirect(`/users/${users.length - 1}`);//redirects to the new user's page
+        res.redirect(`/users/${users.length - 1}`)//redirects to the new user's page
     } else {
-        console.log("error");
-        res.render("users/new", { firstName: req.body.firstName });//redirects back to the form with the data that was entered
+        console.log("error")
+        res.render("users/new", { firstName: req.body.firstName })//redirects back to the form with the data that was entered
     }
-    
 })
 
-router.route("/:id")
-.get((req, res) => {
-    const userID = req.params.id;
-    console.log(req.user)
-    res.send(`get user with id ${userID}`)
-    
-})
-.put((req, res) => {
-    const userID = req.params.id;
-    res.send(`update user with id ${userID}`)
-    
-})
-.delete((req, res) => {
-    const userID = req.params.id;
-    res.send(`delete user with id ${userID}`)
-    
-});
+router
+    .route("/:id")
+    .get((req, res) => {
+        console.log(req.params.id)
+        res.send(`get user with id ${userID}`)
+    })
+    .put((req, res) => {
+        res.send(`update user with id ${req.params.id}`)   
+    })
+    .delete((req, res) => {
+        res.send(`delete user with id ${req.params.id}`)  
+    })
 
-const users = [{name: "John"}, {name: "Jane"}];
-
+const users = [{name: "John"}, {name: "Jane"}]
 router.param("id", (req, res, next, id) => {//runs between the request and the route handler
-    req.user = users[id];
-
-    next();
+    req.user = users[id]
+    next()
 })
 
 function logger(req, res, next) {
-    console.log(req.originalUrl);
-    next();
+    console.log(req.originalUrl)
+    next()
 }
 
-module.exports = router;
+module.exports = router
