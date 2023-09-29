f9e7e0baaa7a8cb34acc346084737bb6583a6d35
Dylan <dylanpjhough@gmail.com>
2023-09-29 10:31:23 +0100

    got rid of uneeded statement

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 app.get("/", (req, res) => {
     
 
-    res.render('index', { text1: "Hello World", text2: "Hello World 2"});
+    res.render('index', { text1: "Hello World"});
     
 
 });
