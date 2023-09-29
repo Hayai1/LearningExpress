1b496374c7020cee89dcb7b1c8c165bff876974e
Dylan <dylanpjhough@gmail.com>
2023-09-29 11:26:05 +0100

    static stuff

--- server.js
+++ server.js
@@ -2,23 +2,14 @@ const express = require("express");
 
 const app = express();
 
+
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-app.get("/", logger, (req, res) => {//runs logger first, then the route handler (just on get requests for this route)
-    
-
-    res.render('index', { text1: "Hello World"});
-    
-
-});
 
 const userRouter = require("./routes/users");
 
 app.use("/users", userRouter);
 
-function logger(req, res, next) {
-    console.log(req.originalUrl);
-    next();
-}
 
 app.listen(3000);
